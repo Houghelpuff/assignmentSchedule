5d573b6240ab1a8fac4c243192a0fbb09e0f85e0
Houghelpuff <jacobhough2017@gmail.com>
2018-11-11 07:53:14 -0700

    Fixed print assignments func

--- schedule_classes.hpp
+++ schedule_classes.hpp
@@ -4,102 +4,98 @@
 
 const int SIZE = 50;
 
+class Assignment {
+ private:
+  std::string toDo;
+  std::string className;
+  int day;
+  int month;
 
+ public:
+  Assignment();
+  Assignment(std::string, std::string, int, int);
+  ~Assignment();
 
-class Assignment{
-  private:
-    std::string toDo;
-    std::string className;
-    int day;
-    int month;
+  std::string getToDo();
+  void setToDo(std::string);
 
-  public:
-    Assignment();
-    Assignment(std::string, std::string, int, int);
-    ~Assignment();
+  std::string getClassName();
+  void setClassName(std::string);
 
-    std::string getToDo();
-    void setToDo(std::string);
+  int getDayDue();
+  void setDayDue(int);
 
-    std::string getClassName();
-    void setClassName(std::string);
-
-    int getDayDue();
-    void setDayDue(int);
-
-    int getMonthDue();
-    void setMonthDue(int);
+  int getMonthDue();
+  void setMonthDue(int);
 };
 
-Assignment::Assignment(){
+Assignment::Assignment() {
   toDo = "";
   className = "";
   day = -1;
   month = -1;
 }
 
-Assignment::Assignment(std::string to, std::string name, int d, int m){
+Assignment::Assignment(std::string to, std::string name, int d, int m) {
   toDo = to;
   className = name;
   day = d;
   month = m;
 }
 
-Assignment::~Assignment(){
+Assignment::~Assignment() {}
 
-}
-
-std::string Assignment::getToDo(){
+std::string Assignment::getToDo() {
   return toDo;
 }
 
-void Assignment::setToDo(std::string str){
+void Assignment::setToDo(std::string str) {
   toDo = str;
 }
 
-std::string Assignment::getClassName(){
+std::string Assignment::getClassName() {
   return className;
 }
 
-void Assignment::setClassName(std::string str){
+void Assignment::setClassName(std::string str) {
   className = str;
 }
 
-int Assignment::getDayDue(){
+int Assignment::getDayDue() {
   return day;
 }
 
-void Assignment::setDayDue(int d){
+void Assignment::setDayDue(int d) {
   day = d;
 }
 
-int Assignment::getMonthDue(){
+int Assignment::getMonthDue() {
   return month;
 }
 
-void Assignment::setMonthDue(int m){
+void Assignment::setMonthDue(int m) {
   month = m;
 }
 
-class AssignmentTracker{
-  private:
-    int front, rear;
-    Assignment assignments[SIZE];
+class AssignmentTracker {
+ private:
+  int front, rear;
+  Assignment assignments[SIZE];
 
-  public:
-    AssignmentTracker(); //done
-    AssignmentTracker(Assignment[]); //done
-    bool isEmpty(); //done
-    void addAssignment(); //done
-    void completeAssignment(); 
-    Assignment peek(int); //done
-    void print(); //done
+ public:
+  AssignmentTracker();              // done
+  AssignmentTracker(Assignment[]);  // done
+  bool isEmpty();                   // done
+  void addAssignment();             // done
+  void completeAssignment();
+  Assignment peek(int);  // done
+  void print();          // done
 };
 
-AssignmentTracker::AssignmentTracker(){
+AssignmentTracker::AssignmentTracker() {
   front = rear = -1;
 
-  for(int i = 0; i < 50; i++){
+  for (int i = 0; i < 50; i++) {
     assignments[i].setToDo("None");
     assignments[i].setClassName("None");
     assignments[i].setDayDue(-1);
@@ -108,33 +104,41 @@ AssignmentTracker::AssignmentTracker(){
   }
 }
 
-AssignmentTracker::AssignmentTracker(Assignment work[]){
-  front = 0;
-  for(int i = 0; i < 50; i++){
-    if(work[i].getDayDue() == -1){
-      rear = i-1;
-      break;
+AssignmentTracker::AssignmentTracker(Assignment work[]) {
+  if (work[0].getDayDue() == -1) {
+    front = rear = -1;
+  }
+
+  else {
+    for (int i = 0; i < 50; i++) {
+      if (work[i].getDayDue() == -1) {
+        rear = i - 1;
+        break;
+      }
+      assignments[i] = work[i];
     }
-    assignments[i] = work[i];
   }
 }
 
-bool AssignmentTracker::isEmpty(){
-  if(front == rear){
+bool AssignmentTracker::isEmpty() {
+  if (front == rear) {
+    std::cout << "Front: " << this->front << " Rear: " << this->rear
+              << std::endl;
     return true;
   }
 
-  else{
+  else {
     return false;
   }
 }
 
-void AssignmentTracker::addAssignment(){
+void AssignmentTracker::addAssignment() {
   std::string forClass;
   std::string what;
   std::string due;
 
   std::cout << "What class is this for?" << std::endl;
+  std::cin.ignore();
   getline(std::cin, forClass);
 
   std::cout << "\nWhat is the assignment" << std::endl;
@@ -145,52 +149,56 @@ void AssignmentTracker::addAssignment(){
 
   std::cout << "\nAssignment added to calender!" << std::endl;
 
-  int d = stoi(due.substr(3,4));
-  int m = stoi(due.substr(0,1));
+  int d = stoi(due.substr(3, 4));
+  int m = stoi(due.substr(0, 1));
 
   Assignment newAssignment(what, forClass, d, m);
 
-  if(isEmpty()){
+  if (isEmpty()) {
     front = rear = 0;
+    std::cout << "Test" << std::endl;
   }
 
-  else{
-    rear = (rear+1)%SIZE;
+  else {
+    rear = (rear + 1) % SIZE;
   }
+
+  std::cout << "Front: " << this->front << " Rear: " << this->rear << std::endl;
 
   assignments[rear] = newAssignment;
-
 }
 
-void AssignmentTracker::print(){
-  for(int i = 0; i < rear; i++){
-    std::cout << assignments[i].getToDo() << " " << assignments[i].getClassName() << " " << assignments[i].getMonthDue() << " " << assignments[i].getDayDue() << std::endl;
+void AssignmentTracker::print() {
+  if (this->isEmpty() == true) {
+    std::cout << "There are no assignments due!" << std::endl;
+  }
+
+  else {
+    for (int i = 0; i < rear + 1; i++) {
+      std::cout << assignments[i].getToDo() << " "
+                << assignments[i].getClassName() << " "
+                << assignments[i].getMonthDue() << "/"
+                << assignments[i].getDayDue() << std::endl;
+    }
   }
 }
 
-Assignment AssignmentTracker::peek(int idx){
+Assignment AssignmentTracker::peek(int idx) {
   return assignments[idx];
 }
 
-void AssignmentTracker::completeAssignment(){
-  if(isEmpty()){
+void AssignmentTracker::completeAssignment() {
+  if (isEmpty()) {
     std::cout << "You have no assignments to complete" << std::endl;
   }
 
-  else if(front == rear){
-      front = rear = -1;
-    }
+  else if (front == rear) {
+    front = rear = -1;
+  }
 
-  else{
-    front = (front+1)%SIZE;
+  else {
+    front = (front + 1) % SIZE;
   }
 }
-
-
-
-
-
-
-
 
 #endif
